97c0e9a523e5aed7a0992e35b90b031eed67b185
Arda Nakisci <anakisci@gmail.com>
2019-07-21 16:56:08 +0300

    add seed to question model

--- src/infra/support/fakers/development/questions.js
+++ src/infra/support/fakers/development/questions.js
@@ -1,9 +1,39 @@
 module.exports = () => {
-  return [{
-    examName: 'LGS',
-    courseName: 'Matematik',
-    subjectName: 'Sayilar',
-    questionLink: 'some_question_link',
-    correctAnswer: 4
-  }]
+  return [
+    {
+      examName: 'LGS',
+      courseName: 'Matematik',
+      subjectName: 'Sayilar',
+      questionLink: 'https://i.vimeocdn.com/portrait/3212476_300x300',
+      correctAnswer: 4
+    },
+    {
+      examName: 'LGS',
+      courseName: 'Matematik',
+      subjectName: 'Sayilar',
+      questionLink: 'https://media.npr.org/assets/img/2019/07/16/ap_19197525742263_wide-7e81b604b4c8bb6908bc70b0de066499a9392a09-s700-c85.jpg',
+      correctAnswer: 1
+    },
+    {
+      examName: 'LGS',
+      courseName: 'Matematik',
+      subjectName: 'Sayilar',
+      questionLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTst0J-Sg97gWBCQ_1K6xbNyxllSlzXPmkYVKOqRe2PGR3Xp5Ev',
+      correctAnswer: 2
+    },
+    {
+      examName: 'LGS',
+      courseName: 'Matematik',
+      subjectName: 'Sayilar',
+      questionLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_hW1G2KikAiGL7aK6NBGNPbO91FG5z_HFh5Sx7a7kq44iiWA7',
+      correctAnswer: 5
+    },
+    {
+      examName: 'LGS',
+      courseName: 'Matematik',
+      subjectName: 'Sayilar',
+      questionLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmpuayj7xEonjJBoaFzHrudgF6gRH8Pr73LnClVH9Vw0raX833',
+      correctAnswer: 3
+    }
+  ]
 }
