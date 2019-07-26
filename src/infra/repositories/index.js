7a7ec9646aeeda574f1ee4fb680d84d39d29ad56
Arda Nakisci <anakisci@gmail.com>
2019-07-26 20:43:52 +0300

    start statisticsa

--- src/infra/repositories/index.js
+++ src/infra/repositories/index.js
@@ -1,9 +1,11 @@
 const User = require('./user')
 const Question = require('./question')
+const Statistic = require('./statistic')
 
 module.exports = ({ database }) => {
   const userModel = database.models.users
   const questionModel = database.models.questions
+  const statisticModel = database.models.statistic
 
   return {
     userRepository: User({ model: userModel }),
