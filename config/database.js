218b104ab14d0e6aa856ea813ef72ed9444de7c1
Arda Nakisci <anakisci@gmail.com>
2019-08-04 14:55:19 +0300

    fix lint

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = {
     url: process.env.DATABASE_URL,
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    dialect: 'postgres',
+    dialect: 'postgres'
     /* ssl: process.env.DATABASE_SSL_ENABLED,
     dialectOptions: {
       ssl: {
