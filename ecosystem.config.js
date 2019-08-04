d61265d2139654348e07af9d1c28837238f88e97
Arda Nakisci <anakisci@gmail.com>
2019-08-04 12:26:50 +0300

    add redis to engine

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+// ecosystem.config.js
+const os = require('os');
+module.exports = {
+    apps: [{
+        port        : process.env.ENGINE_PORT,
+        name        : "colyseus",
+        script      : "src/interfaces/websocket/gameEngine.js", // your entrypoint file
+        watch       : true,           // optional
+        instances   : os.cpus().length,
+        exec_mode   : 'fork',         // IMPORTANT: do not use cluster mode.
+        env: {
+            DEBUG: "colyseus:errors",
+            NODE_ENV: "development",
+        }
+    }]
+}
--- src/interfaces/websocket/gameEngine.js
+++ src/interfaces/websocket/gameEngine.js
@@ -4,12 +4,17 @@ const monitor = require('@colyseus/monitor').monitor
 const roomRegisterService = require('../../domain/gameEngine')
 
 module.exports = ({ logger }) => {
-  const port = process.env.ENGINE_PORT || 5000
+  const port = Number(process.env.ENGINE_PORT) || 5000 + Number(process.env.NODE_APP_INSTANCE)
 
   return {
     start: (app) => new Promise((resolve) => {
       const server = http.createServer(app)
-      const gameEngine = new colyseus.Server({ server })
+      const gameEngine = new colyseus.Server({
+        server,
+        presence: new colyseus.RedisPresence({
+          url: 'redis://127.0.0.1:6379/0'
+        })
+      })
 
       const registeredGameEngine = roomRegisterService(gameEngine)
 
