daba2b73cde8d56bb2cf095c0bd192d56c99015e
Arda Nakisci <anakisci@gmail.com>
2019-08-04 14:54:25 +0300

    add new env

--- config/database.js
+++ config/database.js
@@ -9,12 +9,12 @@ module.exports = {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     dialect: 'postgres',
-    ssl: process.env.DATABASE_SSL_ENABLED,
+    /* ssl: process.env.DATABASE_SSL_ENABLED,
     dialectOptions: {
       ssl: {
         require: process.env.DATABASE_SSL_ENABLED
       }
-    }
+    } */
   },
   development: {
     url: process.env.DATABASE_URL,
---
+++ src/infra/support/fakers/local/users.js
@@ -0,0 +1,47 @@
+const { encryptPassword } = require('src/infra/encryption')
+
+module.exports = () => {
+  const password = encryptPassword('pass')
+
+  return [
+    {
+      id: '48e40a9c-c5e9-4d63-9aba-b77cdf4ca67b',
+      username: 'Test Username',
+      name: 'Test',
+      lastname: 'Developer',
+      email: 'testdev@gmail.com',
+      password: password,
+      isDeleted: 0,
+      city: 'Test City',
+      birthDate: (new Date()).toISOString(),
+      profilePicture: 'Test Profile Picture',
+      coverPicture: 'Test Cover Picture'
+    },
+    {
+      id: '4973ef67-cc68-4702-8082-f9ea6b69a463',
+      username: 'Kullanici1',
+      name: 'kullanici',
+      lastname: '1',
+      email: 'kullanici1@gmail.com',
+      password: password,
+      isDeleted: 0,
+      city: 'sehir',
+      birthDate: (new Date()).toISOString(),
+      profilePicture: 'https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png',
+      coverPicture: 'Test Cover Picture'
+    },
+    {
+      id: 'c4b812f2-78d5-4bc3-a46a-87a03bdf97fc',
+      username: 'Kullanici2',
+      name: 'kullanici',
+      lastname: '2',
+      email: 'kullanici2@gmail.com',
+      password: password,
+      isDeleted: 0,
+      city: 'sehir',
+      birthDate: (new Date()).toISOString(),
+      profilePicture: 'https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png',
+      coverPicture: 'Test Cover Picture'
+    }
+  ]
+}
