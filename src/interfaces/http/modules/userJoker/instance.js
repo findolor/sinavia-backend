cd84a616f886dd6b6ad78fd219b16d54c4588e9d
Arda Nakisci <anakisci@gmail.com>
2019-10-24 12:36:19 +0300

    delete post joker route

--- src/interfaces/http/modules/userJoker/instance.js
+++ src/interfaces/http/modules/userJoker/instance.js
@@ -1,5 +1,5 @@
 const container = require('src/container') // we have to get the DI
-const { getUserJoker, postUserJoker, putUserJoker } = require('src/app/userJoker')
+const { getUserJoker, putUserJoker } = require('src/app/userJoker')
 
 module.exports = () => {
   const {
@@ -8,12 +8,10 @@ module.exports = () => {
   } = container.cradle
 
   const getUserJokerUseCase = getUserJoker({ userJokerRepository, database })
-  const postUserJokerUseCase = postUserJoker({ userJokerRepository })
   const putUserJokerUseCase = putUserJoker({ userJokerRepository })
 
   return {
     getUserJokerUseCase,
-    postUserJokerUseCase,
     putUserJokerUseCase
   }
 }
