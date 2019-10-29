bde46439e184baefc5adc709c7ac9ca9799e1e29
Arda Nakisci <anakisci@gmail.com>
2019-10-29 14:37:26 +0300

    fix route

--- src/interfaces/http/modules/userScore/instance.js
+++ src/interfaces/http/modules/userScore/instance.js
@@ -1,12 +1,14 @@
 const container = require('src/container') // we have to get the DI
 const { getUserScore } = require('src/app/userScore')
+const Sequelize = require('sequelize')
 
 module.exports = () => {
   const {
-    repository: { userScoreRepository }
+    repository: { userScoreRepository },
+    database
   } = container.cradle
 
-  const getUserScoreUseCase = getUserScore({ userScoreRepository })
+  const getUserScoreUseCase = getUserScore({ userScoreRepository, database, Sequelize })
 
   return {
     getUserScoreUseCase
