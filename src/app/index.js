c7619187d63ceff942fbfd4ee50910ed92ad1bd8
Arda Nakisci <anakisci@gmail.com>
2019-11-05 12:35:04 +0300

    add reverse proxy server to app

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,4 @@
-module.exports = ({ server, database, gameEngine, logger, cronJob }) => {
+module.exports = ({ server, database, gameEngine, logger, cronJob, reverseProxy }) => {
   return {
     start: () =>
       Promise
@@ -7,6 +7,10 @@ module.exports = ({ server, database, gameEngine, logger, cronJob }) => {
         // Loads the ongoing matches from database
         .then(cronJob.loadOngoingMatchCrons)
         .then(server.start)
+        .then(reverseProxy.startReverseProxy)
+        // App comes from the reverse proxy server
+        // It runs on 8080 and redirects /api/* to api server
+        // Engine runs on 8080 too
         .then(app => gameEngine.start(app))
         // Gets the whole game content from db at 4 AM and puts it in cache
         .then(cronJob.makeGameContentCronJob)
