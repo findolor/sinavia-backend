ca9ff7a95005c86adcfb8d774ebdbb8b4f1f774d
Arda Nakisci <anakisci@gmail.com>
2019-11-08 11:38:57 +0300

    add pm2 logs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,10 @@ module.exports = {
     {
       name: 'sinavia_app',
       script: 'index.js', // your entrypoint file
+      error_file: './logs/pm2_logs/err.log',
+      out_file: './logs/pm2_logs/out.log',
+      log_file: './logs/pm2_logs/combined.log',
+      time: true,
       instances: 1,
       exec_mode: 'fork', // Must be fork!!
       env: {
