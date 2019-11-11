ebcae666b0b4e0e36d26f83e04ad0f47f7726c8d
Arda Nakisci <anakisci@gmail.com>
2019-11-11 16:03:08 +0300

    make test seeders

--- src/infra/support/fakers/test/users.js
+++ src/infra/support/fakers/test/users.js
@@ -1,21 +1,14 @@
 const { encryptPassword } = require('src/infra/encryption')
+const moment = require('moment')
+moment.locale('tr')
+
+const coverPicture = 'https://firebasestorage.googleapis.com/v0/b/sinavia-deploy-test-258708.appspot.com/o/coverPictures%2FdefaultCoverPicture.png?alt=media&token=0377e5ce-ed5a-45d4-b8a9-661961729d92'
+const profilePicture = 'https://firebasestorage.googleapis.com/v0/b/sinavia-deploy-test-258708.appspot.com/o/profilePictures%2FdefaultProfilePicture.png?alt=media&token=a5e7862f-73b0-4be4-8d59-e8a425da5e1f'
 
 module.exports = () => {
-  const password = encryptPassword('pass')
+  const password = encryptPassword('password')
 
   return [
-    {
-      id: '48e40a9c-c5e9-4d63-9aba-b77cdf4ca67b',
-      username: 'Test Username',
-      name: 'Test',
-      lastname: 'Developer',
-      email: 'testdev@gmail.com',
-      password: password,
-      city: 'Test City',
-      birthDate: new Date(),
-      profilePicture: 'Test Profile Picture',
-      coverPicture: 'Test Cover Picture'
-    },
     {
       id: '4973ef67-cc68-4702-8082-f9ea6b69a463',
       username: 'Kullanici1',
@@ -25,8 +18,10 @@ module.exports = () => {
       password: password,
       city: 'sehir',
       birthDate: new Date(),
-      profilePicture: 'https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png',
-      coverPicture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYsqp6g6oM5OS7VVBPZFfxICh0niuPUx8e8Rl4m-YzlVRkTUHG'
+      deviceId: 'E6559387-A6D2-4CCD-903A-3C8D2F936AD0',
+      profilePicture: profilePicture,
+      coverPicture: coverPicture,
+      isPremium: false
     },
     {
       id: 'c4b812f2-78d5-4bc3-a46a-87a03bdf97fc',
@@ -37,8 +32,50 @@ module.exports = () => {
       password: password,
       city: 'sehir',
       birthDate: new Date(),
-      profilePicture: 'https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png',
-      coverPicture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYsqp6g6oM5OS7VVBPZFfxICh0niuPUx8e8Rl4m-YzlVRkTUHG'
+      deviceId: '6B58287D-49D3-452F-832C-5FFA0DE4EEB8',
+      profilePicture: profilePicture,
+      coverPicture: coverPicture,
+      isPremium: false
+    },
+    {
+      id: 'b46c9693-c17d-498a-adf6-bd618c23f4b3',
+      username: 'Kullanici3',
+      name: 'kullanici',
+      lastname: '3',
+      email: 'kullanici3@gmail.com',
+      password: password,
+      city: 'sehir',
+      birthDate: new Date(),
+      deviceId: '9B2B72B8-4AB3-4522-9D4F-44A357160F6A',
+      profilePicture: profilePicture,
+      coverPicture: coverPicture,
+      isPremium: false
+    },
+    {
+      id: '08f228a0-443b-4003-8b17-efe835cf6916',
+      username: 'Marmelade',
+      name: 'Arda',
+      lastname: 'Nakisci',
+      email: 'littlepossum66@gmail.com',
+      password: password,
+      deviceId: '6B58287D-49D3-452F-832C-5FFA0DE4EEB8',
+      profilePicture: profilePicture,
+      coverPicture: coverPicture,
+      isPremium: true,
+      premiumEndDate: moment().add(1, 'weeks').toDate()
+    },
+    {
+      id: '72b6fa48-94c0-4691-8ac9-db679a67a9b3',
+      username: 'Shellbreaker',
+      name: 'Daar',
+      lastname: 'Kinasci',
+      email: 'anakisci@gmail.com',
+      password: password,
+      deviceId: 'CEAD0717-2323-4621-BB1E-AE9FA5624205',
+      profilePicture: profilePicture,
+      coverPicture: coverPicture,
+      isPremium: true,
+      premiumEndDate: moment().add(1, 'weeks').toDate()
     }
   ]
 }
