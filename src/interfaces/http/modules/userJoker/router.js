470a138466599c608fb5beac472733f1b3891f0c
Arda Nakisci <anakisci@gmail.com>
2019-12-18 16:55:20 +0300

    add reward route to user jokers

--- src/interfaces/http/modules/userJoker/router.js
+++ src/interfaces/http/modules/userJoker/router.js
@@ -66,5 +66,29 @@ module.exports = ({
         })
     })
 
+  router
+    .put('/reward/all/:userId', (req, res) => {
+      getUserJokerUseCase
+        .getJokers({ userId: req.params.userId })
+        .then(data => {
+                      data.forEach(userJoker => {
+            userJoker.amount += 2
+
+            putUserJokerUseCase
+            .updateUserJoker({ userJokerEntity: userJoker })
+            .catch(error => {
+              logger.error(error.stack)
+              res.status(Status.BAD_REQUEST).json(Fail(error.message))
+            })
+          })
+          res.status(Status.OK).json(Success(data))
+        })
+        .catch((error) => {
+          logger.error(error.stack) // we still need to log every error for debugging
+          res.status(Status.BAD_REQUEST).json(
+            Fail(error.message))
+        })
+    })
+
   return router
 }
