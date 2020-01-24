1d48a512d4b05f23bd259eae8a97e12b24148157
Arda Nakisci <anakisci@gmail.com>
2020-01-24 16:47:05 +0300

    const

--- loadtest/gameEngine/rankedRoomTester.js
+++ loadtest/gameEngine/rankedRoomTester.js
@@ -1,5 +1,4 @@
-try {
-  console.time('test')
+console.time('test')
   const Colyseus = require('colyseus.js')
 
   const shellBreaker = '72b6fa48-94c0-4691-8ac9-db679a67a9b3'
@@ -20,80 +19,88 @@ try {
   let totalClientErrors = []
 
   for (let i = 0; i < iterationCount; i++) {
-    setTimeout(() => {
-      // Connect our client to game engine
-      clients[i] = isServerTest ? new Colyseus.Client('ws://35.246.252.239:5000') : new Colyseus.Client('ws://localhost:5000')
-
-      joinOptions = {
-        create: true,
-        examId: 1,
-        courseId: 1,
-        subjectId: 1,
-        databaseId: i % 2 === 0 ? isServerTest === true ? firstIdTestServer : shellBreaker : isServerTest === true ? secondIdTestServer : marmelade
-      }
-      // console.log(joinOptions.databaseId)
-      clients[i].onOpen.add(() => {
-        // Joins a room or creates one with given options
-        rooms[i] = clients[i].join('rankedRoom', joinOptions)
-
-        rooms[i].onJoin.add(() => {
-          console.log(++joined, 'join count')
-          totalJoinedUsers.push(i)
-          // console.log(clients[i])
-
-          // Game state coming from server
-          rooms[i].onStateChange.add(state => {
-            switch (state.rankedState.stateInformation) {
-              case 'question':
-                // Answering the question 2 seconds later
-                setTimeout(() => {
-                  answerQuestion(rooms[i])
-                }, Math.floor(Math.random() * 3000) + 1)
-                break
-              case 'match-finished':
-                console.log(++finishCount, 'finish count')
-                totalFinishedUsers.push(i)
-                rooms[i].leave()
-                break
+    try {
+      setTimeout(() => {
+        // Connect our client to game engine
+        clients[i] = isServerTest ? new Colyseus.Client('ws://35.246.252.239:5000') : new Colyseus.Client('ws://localhost:5000')
+    
+        joinOptions = {
+          create: true,
+          examId: 1,
+          courseId: 1,
+          subjectId: 1,
+          databaseId: i % 2 === 0 ? isServerTest === true ? firstIdTestServer : shellBreaker : isServerTest === true ? secondIdTestServer : marmelade
+        }
+        // console.log(joinOptions.databaseId)
+        clients[i].onOpen.add(() => {
+          // Joins a room or creates one with given options
+          rooms[i] = clients[i].join('rankedRoom', joinOptions)
+    
+          rooms[i].onJoin.add(() => {
+            console.log(++joined, 'join count')
+            totalJoinedUsers.push(i)
+            // console.log(clients[i])
+    
+            // Game state coming from server
+            rooms[i].onStateChange.add(state => {
+              switch (state.rankedState.stateInformation) {
+                case 'question':
+                  // Answering the question 2 seconds later
+                  setTimeout(() => {
+                    answerQuestion(rooms[i])
+                  }, Math.floor(Math.random() * 3000) + 1)
+                  break
+                case 'match-finished':
+                  console.log(++finishCount, 'finish count')
+                  totalFinishedUsers.push(i)
+                  rooms[i].leave()
+                  break
+              }
+            })
+          })
+    
+          rooms[i].onMessage.add(message => {
+            if (message.action === 'game-init') {
+              // We send the server a 'ready' signal to let it know client is ready
+              setTimeout(() => {
+                rooms[i].send({
+                  action: 'ready'
+                })
+              }, 1000)
             }
           })
+    
+          rooms[i].onError.add(error => {
+            console.error(error)
+          })
         })
-
-        rooms[i].onMessage.add(message => {
-          if (message.action === 'game-init') {
-            // We send the server a 'ready' signal to let it know client is ready
-            setTimeout(() => {
-              rooms[i].send({
-                action: 'ready'
-              })
-            }, 1000)
-          }
-        })
-
-        rooms[i].onError.add(error => {
+    
+        clients[i].onError.add(error => {
+          totalClientErrors.push(i)
           console.error(error)
         })
-      })
-
-      clients[i].onError.add(error => {
-        totalClientErrors.push(i)
-        console.error(error)
-      })
-      // Users join in the given amount of time
-    }, Math.floor(Math.random() * 60000) + 1000)
+        // Users join in the given amount of time
+      }, Math.floor(Math.random() * 60000) + 1000)
+    } catch (error) {
+      console.error('\x1b[41m', error)
+    }
   }
 
   function answerQuestion (room) {
-    room.send({
-      action: 'button-press',
-      button: joinOptions.examId === 1 ? Math.floor(Math.random() * 5) + 1 : Math.floor(Math.random() * 4) + 1
-    })
-
-    setTimeout(() => {
+    try {
       room.send({
-        action: 'finished'
+        action: 'button-press',
+        button: joinOptions.examId === 1 ? Math.floor(Math.random() * 5) + 1 : Math.floor(Math.random() * 4) + 1
       })
-    }, 2000)
+  
+      setTimeout(() => {
+        room.send({
+          action: 'finished'
+        })
+      }, 2000)
+    } catch(error) {
+      console.error('\x1b[41m', error)
+    }
   }
 
   process.on('exit', function () {
@@ -109,6 +116,3 @@ try {
     console.log('Total number of client errors: ', totalClientErrors.length)
     console.timeEnd('test')
   })
-} catch (error) {
-  console.error('\x1b[41m', error)
-}
