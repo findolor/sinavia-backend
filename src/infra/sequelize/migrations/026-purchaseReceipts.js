a6056000e61ae0ab64976f0f0ced605bb0508bc1
Arda Nakisci <anakisci@gmail.com>
2020-02-10 13:30:58 +0300

    make receipt field text

--- src/infra/sequelize/migrations/026-purchaseReceipts.js
+++ src/infra/sequelize/migrations/026-purchaseReceipts.js
@@ -19,7 +19,7 @@ module.exports = {
         onDelete: 'CASCADE'
       },
       receipt: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         allowNull: false
       },
       createdAt: {
