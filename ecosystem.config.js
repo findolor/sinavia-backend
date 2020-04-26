533522f36fc0e9956b9454208ec729bb8f98b054
Arda Nakisci <anakisci@gmail.com>
2020-04-26 11:15:35 +0300

    remove pm2 logs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,9 @@ module.exports = {
     {
       name: 'sinavia_app',
       script: 'index.js', // your entrypoint file
-      error_file: './logs/pm2_logs/err.log',
-      out_file: './logs/pm2_logs/out.log',
-      log_file: './logs/pm2_logs/combined.log',
+      error_file: 'dev/null',
+      out_file: 'dev/null',
+      log_file: 'dev/null',
       time: true,
       instances: -1,
       exec_mode: 'fork', // Must be fork!!
